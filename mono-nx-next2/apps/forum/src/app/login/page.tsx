03f6a779c1a0a38ce6b21fa9b7f2c5fd57023953
PavelPoroskov <pavel.poroskov.se@gmail.com>
2025-01-15 21:36:52 +0300

    mono-nx-next2: implement auth-component

--- mono-nx-next2/apps/forum/src/app/login/page.tsx
+++ mono-nx-next2/apps/forum/src/app/login/page.tsx
@@ -1,9 +1,11 @@
+import { AuthComponent } from '@mono-nx-next2/lib-ui';
 import styles from './page.module.css';
 
 export default function Login() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Login!</h1>
+      <AuthComponent />
     </div>
   );
 }
--- mono-nx-next2/lib/lib-ui/src/lib/auth-component/auth-component.tsx
+++ mono-nx-next2/lib/lib-ui/src/lib/auth-component/auth-component.tsx
@@ -1,9 +1,34 @@
 import styles from './auth-component.module.css';
+import { useState } from 'react'
 
 export function AuthComponent() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Handle login logic here
+  };
+
   return (
     <div className={styles['container']}>
-      <h1>Welcome to AuthComponent!</h1>
+      <h1>Login to Y Organization</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+        id="username"
+        className={styles['input']}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        id="password"
+        className={styles['input']}
+      />
+      <button onClick={handleLogin} id="login-button" className={styles['button']}>Login</button>
     </div>
   );
 }
