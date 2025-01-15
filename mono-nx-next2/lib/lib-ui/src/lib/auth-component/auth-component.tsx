e3165cdbac6e3d10f4067038eaac0d0daf14c9b8
PavelPoroskov <pavel.poroskov.se@gmail.com>
2025-01-15 21:18:42 +0300

    mono-nx-next2: scaffolding auth-component in lib/lib-ui

---
+++ mono-nx-next2/lib/lib-ui/src/lib/auth-component/auth-component.tsx
@@ -0,0 +1,11 @@
+import styles from './auth-component.module.css';
+
+export function AuthComponent() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to AuthComponent!</h1>
+    </div>
+  );
+}
+
+export default AuthComponent;
