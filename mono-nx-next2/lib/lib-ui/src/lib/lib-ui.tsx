707156a2c0ee0f7c0d809a82db2c3d04480a0991
PavelPoroskov <pavel.poroskov.se@gmail.com>
2025-01-15 21:16:27 +0300

    mono-nx-next2: scaffolding lib/lib-ui

---
+++ mono-nx-next2/lib/lib-ui/src/lib/lib-ui.tsx
@@ -0,0 +1,11 @@
+import styles from './lib-ui.module.css';
+
+export function LibUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to LibUi!</h1>
+    </div>
+  );
+}
+
+export default LibUi;
