965836ad62343ceeac85aacf9deb440d2d7e5f49
PavelPoroskov <pavel.poroskov.se@gmail.com>
2025-01-15 20:44:25 +0300

    mono-nx-next2: scaffolding login page

---
+++ mono-nx-next2/apps/list/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function Login() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Login!</h1>
+    </div>
+  );
+}
